b91fe5c3e9a02f7827d791a8b205228e9748e07e
martinbrumana <martinbrumana@gmail.com>
2022-04-20 22:18:23 -0300

    init 420

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hola Coders!,</Text>
+      <Text style={styles.text}>Como andan amigos?</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  }
 });
